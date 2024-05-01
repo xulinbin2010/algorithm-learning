8a5cd18a06a0fcb867da3628d15062ed34d36682
xulinbin <linbing.xu@carrier.com>
2024-05-01 11:47:06 +0800

    添加0/1背包问题解决方案，使用动态规划算法。

---
+++ src/Knapsack01.java
@@ -0,0 +1,28 @@
+package src;
+
+public class Knapsack01 {
+
+    public static int knapsack(int[] weights, int[] values, int capacity) {
+        int n = weights.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= capacity; w++) {
+                if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[n][capacity];
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {2, 3, 4, 5};
+        int[] values = {3, 4, 5, 6};
+        int capacity = 7;
+        System.out.println("Maximum value that can be obtained: " + knapsack(weights, values, capacity));
+    }
+}
